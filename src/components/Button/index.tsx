c50f40bce28ff9def837b2940beb50020ab56458
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2023-05-22 11:01:25 -0300

    Add button html attributes

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,21 +1,21 @@
 import styles from './styles.module.scss'
-
-export interface StyledButtonProps {
+import { ButtonHTMLAttributes } from 'react'
+export interface StyledButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
   color: 'primary' | 'secondary' | 'highligh';
   size: 'small' | 'medium';
   width?: number;
-  children?: React.ReactNode;
 }
 
-function Button ({ color, width, size, children }: StyledButtonProps) {
+function Button({color, width, size, children, ...rest}: StyledButtonProps ) {
   return (
     <button 
       className={`${styles.default} ${styles[size]} ${styles[color]}`}
       style={{width: width ? `${width}px` : 'initial'}}
+      {...rest}
     >
       {children}
     </button>
-  );
+  )
 }
 
 export default Button;
