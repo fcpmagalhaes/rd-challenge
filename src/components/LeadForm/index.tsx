abbdb5d1b9fb1a144f1c86ac2f3339b3ac701f90
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2023-05-25 03:08:46 -0300

    Add placeholder and create list of items to form

--- src/components/LeadForm/index.tsx
+++ src/components/LeadForm/index.tsx
@@ -3,8 +3,49 @@ import styles from "./styles.module.scss";
 import Button from "@/components/Button";
 import { useState } from 'react';
 
+interface ItemTypes {
+    label: string;
+    name: string;
+    message: string;
+    placeholder: string;
+}
+
+const items = [
+    {
+        label: "Diga, qual seu nome?",
+        name: "name",
+        message: 'Por favor digite seu nome',
+        placeholder: "Insira seu nome"
+    },
+    {
+        label: "Seu email de trabalho",
+        name: "email",
+        message: 'Por favor insira seu em-email',
+        placeholder: "Insira seu em-email"
+    },
+    {
+        label: "Seu telefone",
+        name: "phone",
+        message: 'Por favor digite seu telefone!',
+        placeholder: "Insira seu número de telefone com DDD"
+    },
+    {
+        label: "Crie uma senha",
+        name: "password",
+        message: 'Por favor crie uma senha!',
+        placeholder: "Utilize maiúsculas, minúsculas e caracteres especiais"
+    },
+    {
+        label:"Confirme sua senha",
+        name:"passwordConf",
+        message: 'Por favor confirme sua senha!',
+        placeholder: "Confirmação de senha"
+    }
+];
+
 const LeadForm = () => {
-const onFinish = (values: any) => {
+    
+  const onFinish = (values: any) => {
     console.log('Success:', values);
   };
   
@@ -34,75 +75,49 @@ const onFinish = (values: any) => {
             autoComplete="off"
             className={styles.form}
         >
-            <Form.Item
-            label="Diga, qual seu nome?"
-            name="name"
-            rules={[{ required: true, message: 'Por favor digite seu nome' }]}
-            className={styles.item}
+            {
+                items.map((item: ItemTypes, index: number) => (
+                    <Form.Item
+                        label={item.label}
+                        name={item.name}
+                        rules={[{ required: true, message: item.message }]}
+                        className={styles.item}
+                        key={index}
+                    >
+                        <Input className={styles.input} placeholder={item.placeholder}/>
+                    </Form.Item>
+                ))
+            }
+            <Form.Item 
+                label="Qual o site da sua empresa?"
+                name="remember"
+                className={styles.item}
             >
-            <Input className={styles.input}/>
+                <Checkbox
+                    checked={componentDisabled}
+                    onChange={(e) => setComponentDisabled(e.target.checked)}
+                >
+                    Meu site é
+                </Checkbox>
             </Form.Item>
 
             <Form.Item
-            label="Seu email de trabalho"
-            name="email"
-            rules={[{ required: true, message: 'Por favor digite seu email!' }]}
-            className={styles.item}
+                name="siteURL"
+                className={styles.item}
             >
-            <Input className={styles.input}/>
+                <Input className={styles.input} disabled={!componentDisabled} placeholder='Insira o endereço do seu site'/>
             </Form.Item>
 
             <Form.Item
-            label="Seu telefone"
-            name="phone"
-            rules={[{ required: true, message: 'Por favor digite seu telefone!' }]}
-            className={styles.item}
+                className={styles.item}
+                name="noSite"
             >
-            <Input className={styles.input}/>
-            </Form.Item>
-
-            <Form.Item
-            label="Crie uma senha"
-            name="password"
-            rules={[{ required: true, message: 'Por favor crie uma senha!' }]}
-            className={styles.item}
-            >
-            <Input.Password className={styles.input}/>
-            </Form.Item>
-
-            <Form.Item
-            label="Confirme sua senha"
-            name="passwordConf"
-            rules={[{ required: true, message: 'Por favor confirme sua senha!' }]}
-            className={styles.item}
-            >
-            <Input.Password className={styles.input}/>
-            </Form.Item>
-
-
-            <Form.Item
-            className={styles.item}
-            label="Qual o site da sua empresa?"
-            name="haveSite" valuePropName="checked">
-            <Checkbox
-                onChange={(e) => setComponentDisabled(e.target.checked)}
-            >
-                Meu site é
-            </Checkbox>
-            </Form.Item>
-
-            <Form.Item
-            name="siteURL"
-            // disabled={componentDisabled}
-            className={styles.item}
-            >
-            <Input className={styles.input}/>
-            </Form.Item>
-
-            <Form.Item
-            className={styles.item}
-            name="noSite" valuePropName="uncheked">
-            <Checkbox>Ainda não tenho site</Checkbox>
+                <Checkbox
+                    checked={!componentDisabled}
+                    onChange={(e) => setComponentDisabled(!e.target.checked)}
+                >
+                    Ainda não tenho site
+                </Checkbox>
             </Form.Item>
 
             <div className={styles.buttonContainer}>
