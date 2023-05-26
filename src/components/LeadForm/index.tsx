9cbcc06d8bacdf384f34d020454eb5f37d162abf
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2023-05-26 19:57:38 -0300

    Add rules to input form

--- src/components/LeadForm/index.tsx
+++ src/components/LeadForm/index.tsx
@@ -1,138 +1,204 @@
-import { Checkbox, Form, Input, InputNumber } from 'antd';
+import { Checkbox, Form, Input, Select } from "antd";
 import styles from "./styles.module.scss";
 import Button from "@/components/Button";
-import { useState } from 'react';
+import { useState } from "react";
 
-interface ItemTypes {
-    label: string;
-    name: string;
-    message: string;
-    placeholder: string;
-}
+const { Option } = Select;
 
-const items = [
-    {
-        label: "Diga, qual seu nome?",
-        name: "name",
-        message: 'Por favor digite seu nome',
-        placeholder: "Insira seu nome"
-    },
-    {
-        label: "Seu email de trabalho",
-        name: "email",
-        message: 'Por favor insira seu em-email',
-        placeholder: "Insira seu em-email"
-    },
-    {
-        label: "Seu telefone",
-        name: "phone",
-        message: 'Por favor digite seu telefone!',
-        placeholder: "Insira seu número de telefone com DDD"
-    },
-    {
-        label: "Crie uma senha",
-        name: "password",
-        message: 'Por favor crie uma senha!',
-        placeholder: "Utilize maiúsculas, minúsculas e caracteres especiais"
-    },
-    {
-        label:"Confirme sua senha",
-        name:"passwordConf",
-        message: 'Por favor confirme sua senha!',
-        placeholder: "Confirmação de senha"
-    }
+const positions = [
+  "Sócio(a) / CEO / Proprietário(a)",
+  "Diretor(a) de Vendas",
+  "Diretor(a) de Marketing",
+  "Diretor(a) Outras Áreas",
+  "Gerente de Marketing",
+  "Gerente de Vendas",
+  "Coordenador(a)/Supervisor(a) de Marketing",
+  "Coordenador(a)/Supervisor(a) de Vendas",
+  "Analista/Assistente de Marketing",
+  "Analista/Assistente de Vendas",
+  "Vendedor(a) / Executivo(a) de Contas",
+  "Estudante",
+  "Outros Cargos",
 ];
 
 const LeadForm = () => {
-    
+  const [form] = Form.useForm();
+
   const onFinish = (values: any) => {
-    console.log('Success:', values);
+    console.log("Success:", values);
   };
-  
+
   const onFinishFailed = (errorInfo: any) => {
-    console.log('Failed:', errorInfo);
+    console.log("Failed:", errorInfo);
+  };
+
+  const validatePassword = (_: any, value: string) => {
+    if (!/(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{6,10}$/.test(value)) {
+      return Promise.reject(
+        "A senha precisa ter de 6 a 10 caracteres, uma letra maiúscula e uma minúscula"
+      );
+    }
+    return Promise.resolve();
   };
 
   const [componentDisabled, setComponentDisabled] = useState<boolean>(true);
-  
+
+  const handleCheckboxChange = (e: any) => {
+    setComponentDisabled(e);
+    if (componentDisabled) {
+      form.setFieldsValue({ siteURL: undefined });
+    }
+  };
+
   return (
     <div className={styles.container}>
-        <div className={styles.formHeader}>
-            <div className={styles.title}>
-                Comece seus 10 dias de teste grátis do RD Station Marketing!
-            </div>
-            <div className={styles.subtitle}>
-                Não precisa cadastrar cartão de crédito. 😉
-            </div>
+      <div className={styles.formHeader}>
+        <div className={styles.title}>
+          Comece seus 10 dias de teste grátis do RD Station Marketing!
         </div>
-        
-        <Form
-            layout='vertical'
-            name="basic"
-            initialValues={{ remember: true }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
-            className={styles.form}
+        <div className={styles.subtitle}>
+          Não precisa cadastrar cartão de crédito. 😉
+        </div>
+      </div>
+
+      <Form
+        layout="vertical"
+        name="basic"
+        initialValues={{ site: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+        className={styles.form}
+        form={form}
+      >
+        <Form.Item
+          label="Diga, qual seu nome?"
+          name="name"
+          rules={[{ required: true, message: "Por favor digite seu nome" }]}
+          className={styles.item}
         >
+          <Input className={styles.input} placeholder="Insira seu nome" />
+        </Form.Item>
+        <Form.Item
+          label="Seu email de trabalho"
+          name="email"
+          rules={[
             {
-                items.map((item: ItemTypes, index: number) => (
-                    <Form.Item
-                        label={item.label}
-                        name={item.name}
-                        rules={[{ required: true, message: item.message }]}
-                        className={styles.item}
-                        key={index}
-                    >
-                        <Input className={styles.input} placeholder={item.placeholder}/>
-                    </Form.Item>
-                ))
-            }
-            <Form.Item 
-                label="Qual o site da sua empresa?"
-                name="remember"
-                className={styles.item}
-            >
-                <Checkbox
-                    checked={componentDisabled}
-                    onChange={(e) => setComponentDisabled(e.target.checked)}
-                >
-                    Meu site é
-                </Checkbox>
-            </Form.Item>
+              required: true,
+              message: "Por favor insira seu em-email",
+              type: "email",
+            },
+          ]}
+          className={styles.item}
+        >
+          <Input className={styles.input} placeholder="Insira seu em-email" />
+        </Form.Item>
+        <Form.Item
+          label="Seu telefone"
+          name="phone"
+          rules={[
+            { required: true, message: "Por favor digite seu telefone!" },
+          ]}
+          className={styles.item}
+        >
+          <Input
+            className={styles.input}
+            placeholder="Insira seu número de telefone com DDD"
+          />
+        </Form.Item>
+        <Form.Item
+          label="Seu Cargo de ocupação"
+          name="ocupation"
+          rules={[{ required: true, message: "Por favor digite seu nome" }]}
+          className={styles.item}
+        >
+          <Select placeholder="Selecione seu Cargo" className={styles.input}>
+            {positions.map((position: string, index: number) => (
+              <Option value={index} key={index}>
+                {position}
+              </Option>
+            ))}
+          </Select>
+        </Form.Item>
+        <Form.Item
+          label="Crie uma senha"
+          name="password"
+          rules={[
+            { required: true, message: "Por favor, crie uma senha!" },
+            { validator: validatePassword },
+          ]}
+          className={styles.item}
+        >
+          <Input.Password
+            className={styles.input}
+            placeholder="Utilize maiúsculas, minúsculas e números"
+          />
+        </Form.Item>
+        <Form.Item
+          label="Confirme sua senha"
+          name="passwordConf"
+          dependencies={["password"]}
+          rules={[
+            {
+              required: true,
+              message: "Por favor, confirme uma senha!",
+            },
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (!value || getFieldValue("password") === value) {
+                  return Promise.resolve();
+                }
+                return Promise.reject(
+                  new Error("As duas senhas digitadas são diferentes!")
+                );
+              },
+            }),
+          ]}
+          className={styles.item}
+        >
+          <Input.Password
+            className={styles.input}
+            placeholder="Utilize maiúsculas, minúsculas e números"
+          />
+        </Form.Item>
+        <Form.Item
+          label="Qual o site da sua empresa?"
+          name="site"
+          className={styles.item}
+        >
+          <Checkbox
+            checked={componentDisabled}
+            onChange={(e) => handleCheckboxChange(e.target.checked)}
+          >
+            Meu site é
+          </Checkbox>
+        </Form.Item>
+        <Form.Item
+          name="siteURL"
+          className={styles.item}
+          rules={[{ type: "url", message: "Digite uma URL válida!" }]}
+        >
+          <Input
+            className={styles.input}
+            disabled={!componentDisabled}
+            placeholder="Insira o endereço do seu site"
+          />
+        </Form.Item>
+        <Form.Item className={styles.item} name="noSite">
+          <Checkbox
+            checked={!componentDisabled}
+            onChange={(e) => handleCheckboxChange(!e.target.checked)}
+          >
+            Ainda não tenho site
+          </Checkbox>
+        </Form.Item>
 
-            <Form.Item
-                name="siteURL"
-                className={styles.item}
-            >
-                <Input className={styles.input} disabled={!componentDisabled} placeholder='Insira o endereço do seu site'/>
-            </Form.Item>
-
-            <Form.Item
-                className={styles.item}
-                name="noSite"
-            >
-                <Checkbox
-                    checked={!componentDisabled}
-                    onChange={(e) => setComponentDisabled(!e.target.checked)}
-                >
-                    Ainda não tenho site
-                </Checkbox>
-            </Form.Item>
-
-            <div className={styles.buttonContainer}>
-                <Button
-                    color="highligh"
-                    size="medium"
-                    // onClick={() => "submit"}
-                    onClick={() => {
-                        console.log("cliquei");
-                    }}
-                    >
-                    Criar minha conta
-                </Button>
-            </div>
-        </Form>
+        <div className={styles.buttonContainer}>
+          <Button color="highligh" size="medium">
+            Criar minha conta
+          </Button>
+        </div>
+      </Form>
     </div>
   );
 };
